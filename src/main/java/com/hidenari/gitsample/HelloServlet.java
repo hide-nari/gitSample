3c8aae835e2cc13158a660db85e226e93ef42b47
hide-nari <hide3@mild.ocn.ne.jp>
2026-03-22 13:45:47 +0900

    Add three

--- src/main/java/com/hidenari/gitsample/HelloServlet.java
+++ src/main/java/com/hidenari/gitsample/HelloServlet.java
@@ -22,6 +22,7 @@ public class HelloServlet extends HttpServlet {
         out.println("<h1>" + message + "</h1>");
         out.println("<h1>" + "one" + "</h1>");
         out.println("<h1>" + "two" + "</h1>");
+        out.println("<h1>" + "three" + "</h1>");
         out.println("</body></html>");
     }
 
